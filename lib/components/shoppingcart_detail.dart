6dbb4e5d33a7e04dec84a0b2373004f96204f6b1
jinsa <jinsa004@gmail.com>
2022-10-17 09:51:50 +0900

    버튼 호버색 주입 및 함수설정

--- lib/components/shoppingcart_detail.dart
+++ lib/components/shoppingcart_detail.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingcart/style.dart';
 
 class ShoppingCartDetail extends StatelessWidget {
   const ShoppingCartDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Column(
+        children: [
+          _bulidDetailNameAndPrice(),
+          _bulidDetailRatingAndReviewCount(),
+          _bulidDetailColorOptions(),
+          _bulidDetailButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _bulidDetailNameAndPrice() {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Urban Soft AL 10.0",
+            style: textBody(),
+          ),
+          Text(
+            "\$699",
+            style: textBody(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _bulidDetailRatingAndReviewCount() {
+    return SizedBox();
+  }
+
+  Widget _bulidDetailColorOptions() {
+    return SizedBox();
+  }
+
+  Widget _bulidDetailIcon() {
+    return SizedBox();
+  }
+
+  Widget _bulidDetailButton() {
     return SizedBox();
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+TextStyle textBody() {
+  return TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+}
